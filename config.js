9ded0bf4aa4306f5fc0cd2e0c8d806438911b7ca
Ju Yuhan <154795446+JuYuhan@users.noreply.github.com>
2023-12-25 08:59:27 +0000

    Update 8 files

---
+++ config.js
@@ -0,0 +1,75 @@
+/*
+* https://rentry.org/teralomaniac_clewd
+* https://github.com/teralomaniac/clewd
+*/
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+    "Cookie": "SET YOUR COOKIE HERE",
+    "CookieArray": [
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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"
+    ],
+    "Cookiecounter": 1,
+    "CookieIndex": 0,
+    "ProxyPassword": "85783845",
+    "Ip": "127.0.0.1",
+    "Port": 8444,
+    "localtunnel": false,
+    "BufferSize": 1,
+    "SystemInterval": 3,
+    "rProxy": "",
+    "api_rProxy": "",
+    "padtxt_placeholder": "",
+    "PromptExperimentFirst": "",
+    "PromptExperimentNext": "",
+    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
+    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
+    "Settings": {
+        "RenewAlways": true,
+        "RetryRegenerate": false,
+        "PromptExperiments": true,
+        "SystemExperiments": true,
+        "PreventImperson": false,
+        "AllSamples": false,
+        "NoSamples": false,
+        "StripAssistant": false,
+        "StripHuman": false,
+        "PassParams": false,
+        "ClearFlags": true,
+        "PreserveChats": false,
+        "LogMessages": true,
+        "FullColon": true,
+        "padtxt": 15000,
+        "xmlPlot": true,
+        "Superfetch": true
+    }
+}
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
